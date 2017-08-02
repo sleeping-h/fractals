1ea31c945035aa178813cf97765fa6a0da6d6e5d
Sleeping <hh.sleeping@gmail.com>
2017-08-02 21:44:42 +0300

    whitespace is scary

--- interactive_julia_set.py
+++ interactive_julia_set.py
@@ -1,55 +1,54 @@
 from __future__ import division
 import pygame
 from pygame.locals import *
-pygame.init()
-import cmath
 
-def iterate(z,c,k=0):
-    step=536.842/(k+13.42)
-    if k>255:
+def iterate(z, c, k=0):
+    step = 536.842 / (k + 13.42)
+    if k > 255:
         return 255 
-    elif abs(z)>2:
-        return int(k*k/255)
-    return iterate(z**2+c,c,k+step)
+    elif abs(z) > 2:
+        return int(k * k / 255)
+    return iterate(z ** 2 + c, c, k + step)
 
-scale=80
-mx0,jx0,y0=-2.5,-1.9,-1.5
+scale = 80
+mx0, jx0, y0 = -2.5, -1.9, -1.5
 
-W,H=600,-int(2*y0*scale)
-jW,jH=300,H
-jX,jY=0,0
-mW,mH=W-jW,H
-mX,mY=jX+jW,0
+W, H = 600, -int(2 * y0 * scale)
+jW, jH = 300, H
+jX, jY = 0, 0
+mW, mH = W - jW, H
+mX, mY = jX + jW, 0
 
-screen=pygame.display.set_mode((W,H),0,32)
-screen.fill((0,0,0))
-pygame.draw.rect(screen,(64,64,64),(mX,0,6,H),0)
+pygame.init()
+screen = pygame.display.set_mode((W, H), 0, 32)
+screen.fill((0, 0, 0))
+pygame.draw.rect(screen, (64, 64, 64), (mX, 0, 6, H), 0)
 pygame.display.update()
 
 
-for x in range(7,mW): # Mandelbrot
+for x in range(7, mW): # Mandelbrot
     for y in range(mH):
-        c=x/scale+mx0+1j*(y/scale+y0)
-        k=iterate(0,c)
-        screen.set_at((x+mX,H-y+mY),(k,k,k))
+        c = x / scale + mx0 + 1j * (y / scale + y0)
+        k = iterate(0, c)
+        screen.set_at((x + mX, H - y + mY), (k, k, k))
     pygame.display.update()
 
-mainloop=True
+mainloop = True
 while mainloop:
     for event in pygame.event.get():
-        if event.type==QUIT:
-            mainloop=False
-    x,y=pygame.mouse.get_pos()
-    cx=(x-mX)/scale+mx0
-    cy=(H-y-mY)/scale+y0
-    c=cx+cy*1j
+        if event.type == QUIT:
+            mainloop = False
+    x, y = pygame.mouse.get_pos()
+    cx = (x - mX) / scale + mx0
+    cy = (H - y - mY) / scale + y0
+    c = cx + cy * 1j
     for x in range(jW): # Julia
         for y in range(jH):
-            k=iterate(x/scale+jx0+(y/scale+y0)*1j,c)
-            screen.set_at((x+jX,H-y+jY),(k,k,k))
-    t=pygame.font.SysFont("monospace", 14)
-    text="C=%s+%si" % (round(cx,3),round(cy,3))
-    t1=t.render(text,0,(128,128,128))
-    screen.blit(t1,(0,0)) 
+            k = iterate(x / scale + jx0 + (y / scale + y0) * 1j, c)
+            screen.set_at((x + jX, H - y + jY), (k, k, k))
+    t = pygame.font.SysFont("monospace", 14)
+    text = "C = %s + %si" % (round(cx, 3), round(cy, 3))
+    t1 = t.render(text, 0, (128, 128, 128))
+    screen.blit(t1, (0, 0)) 
     pygame.display.update()
 pygame.quit()
--- tree.py
+++ tree.py
@@ -1,46 +1,46 @@
 from __future__ import division
 import pygame
-from pygame.locals import *
-from math import sin,cos,atan,pi
+from pygame.locals import  * 
+from math import sin, cos, atan, pi
 pygame.init()
 
-W,H=700,600
-C=(W//2,H//2)
-white=(255,255,255)
-black=(0,0,0)
-screen=pygame.display.set_mode((W,H),0,32)
+W, H = 700, 600
+C = (W // 2, H // 2)
+white = (255, 255, 255)
+black = (0, 0, 0)
+screen = pygame.display.set_mode((W, H), 0, 32)
 screen.fill(white) 
-phase=lambda (x1,y1),(x2,y2): atan((y1-y2)/(x2-x1+0.1))+pi*(x2-x1<0)
+phase = lambda (x1, y1), (x2, y2): atan((y1 - y2) / (x2 - x1 + 0.1)) + pi * (x2 - x1 < 0)
     
-def rotate((x1,y1),(x0,y0),l):
-    """rotation CCW, angle l, around x0,y0"""
-    return ((x1-x0)*cos(l)-(y1-y0)*sin(l)+x0,
-            (x1-x0)*sin(l)+(y1-y0)*cos(l)+y0)
+def rotate((x1, y1), (x0, y0), l):
+    """rotation CCW,  angle l,  around x0, y0"""
+    return ((x1 - x0) * cos(l) - (y1 - y0) * sin(l) + x0, 
+            (x1 - x0) * sin(l) + (y1 - y0) * cos(l) + y0)
         
-def line((x1,y1),(x2,y2),w):
-    points=[(int(x1),int(H-y1)),(int(x2),int(H-y2))]
-    pygame.draw.lines(screen,black,False,points,w//2)
+def line((x1, y1), (x2, y2), w):
+    points = [(int(x1), int(H - y1)), (int(x2), int(H - y2))]
+    pygame.draw.lines(screen, black, False, points, w // 2)
     
-def tree(P1,P2,a,n):
-    r=0.33 # shortening of next branch, nice at [0.31..0.48]
-    (x1,y1),(x2,y2)=P1,P2
+def tree(P1, P2, a, n):
+    r = 0.33 # shortening of next branch,  nice at [0.31..0.48]
+    (x1, y1), (x2, y2) = P1, P2
     if n:
-        line(P1,P2,n)
-        P3=(x1+(x2-x1)*r,y1+(y2-y1)*r)
-        tree(P2,rotate(P3,P2,a),a,n-1)
-        tree(P2,rotate(P3,P2,a+pi/2),a,n-1)
+        line(P1, P2, n)
+        P3 = (x1 + (x2 - x1) * r, y1 + (y2 - y1) * r)
+        tree(P2, rotate(P3, P2, a), a, n - 1)
+        tree(P2, rotate(P3, P2, a + pi / 2), a, n - 1)
     else:                                                             
-        pygame.draw.circle(screen,(0,180,20),(int(x1),int(H-y1)),3,1)   
+        pygame.draw.circle(screen, (0, 180, 20), (int(x1), int(H - y1)), 3, 1)   
 
-mainloop=True
+mainloop = True
 while mainloop:
-    M=pygame.mouse.get_pos()
-    tree((W//2,40),(W//2,220),phase(M,C)-2.5,11) # coords of stem
+    M = pygame.mouse.get_pos()
+    tree((W // 2, 40), (W // 2, 220), phase(M, C) - 2.5, 11) # coords of stem
     pygame.display.update()
     screen.fill(white)
     for event in pygame.event.get():
-        if event.type==QUIT:
-            mainloop=False
+        if event.type  ==  QUIT:
+            mainloop = False
 ##    pygame.time.wait(20) # delay
 pygame.quit()
 
